45eb44b3e7a5a0f0de47e183b966e6c2006cde29
unknown <juanito2103@hotmail.es>
2025-02-09 08:20:23 -0500

    primer commit

---
+++ ejercicio.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class ejercicio {
+
+    public static void main(String[] args) {
+        int x,y,z;
+
+        x=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer numero"));
+        y=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo numero"));
+        z=x+y;
+        JOptionPane.showMessageDialog(null,"El resultado es "+z);
+    }   
+}
