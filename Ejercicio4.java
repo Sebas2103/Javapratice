962291e43deacf5cf1664664bb9eeb5029e784c0
unknown <juanito2103@hotmail.es>
2025-02-09 08:39:37 -0500

    tercer commit

---
+++ Ejercicio4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        System.out.println("Ingrese un numero");
+        x = sc.nextInt();
+
+        if (x > 0) {
+            y = 2 * x;
+
+        } else {
+            y=x*x;
+
+        }
+        System.out.println("El resultado es: "+y);
+
+    }
+}
