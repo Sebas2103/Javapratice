552f8c24acdaca589e46e34fb7b227ee37f10dbf
unknown <juanito2103@hotmail.es>
2025-02-09 08:26:32 -0500

    segundo commit

---
+++ ejercicio2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
+        int x,y,z;
+        System.out.println("Ingrese el primer numero ");//instruccion  de salida
+        x=sc.nextInt();//lectura de la variable 
+        System.out.println("Ingrese el segundo  numero ");//instruccion  de salida
+        y=sc.nextInt();//lectura de la variable 
+        z=x+y;
+        System.out.println("El resultado es "+z);
+        
+    }
+}
