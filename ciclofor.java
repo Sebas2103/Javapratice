18300b584e2ee6892aa28c519f0e9f2b950ce1ba
unknown <juanito2103@hotmail.es>
2025-02-10 09:48:43 -0500

    doceavo commit

--- ciclofor.java
+++ ciclofor.java
@@ -1,12 +1,22 @@
 import java.util.Scanner;
-//Diseñar un algoritmo que calcule la  sumade n numeros ingresados por teclado. El usuario deve especificar elvalor de n 
+
+//Diseñar un algoritmo que calcule la  sumade n numeros ingresados por teclado. El usuario debespecificar elvalor de n 
 public class ciclofor {
 
-
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n ;
-        float num,sum=0;
-        sc.close();
+        Scanner sc = new Scanner(System.in);
+        int n;
+        float num, sum = 0;
+        System.out.println("Ingrese la cantidad");
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("ingrese un numero");
+            num = sc.nextFloat();
+            sum = sum + num;
+
+        }
+        System.out.println("la suma de los numeros ingresados es " + sum);
+
     }
+
 }
