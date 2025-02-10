303f43a3bf30729afeaf709a3485c9843bf5334e
unknown <juanito2103@hotmail.es>
2025-02-10 09:18:36 -0500

    octavo commit

---
+++ cicloCuadro.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class cicloCuadro {
+    public static void main(String[] args) {
+        int x = 1;
+        String numeros = "";
+    }
+}
--- ciclos.java
+++ ciclos.java
@@ -1,13 +1,13 @@
 public class ciclos {
-    //Diseñar unalgoritmo que cuente  del 1 al 10
+    // Diseñar unalgoritmo que cuente del 1 al 10
 
-     public static void main(String[] args) {
-        int x=1;
-        while(x<=10){
+    public static void main(String[] args) {
+        int x = 1;
+        while (x <= 10) {
             System.out.println(x);
-            x=x+1;
+            x = x + 1;
 
         }
-        
-     }
+
+    }
 }
