9a42be0217fcb127beef75ab41ace81e1fc1b57b
unknown <juanito2103@hotmail.es>
2025-02-10 11:14:44 -0500

    catorceavo commit

---
+++ aregloEstatico.java
@@ -0,0 +1,13 @@
+public class aregloEstatico {
+    // Son del mismo tipo de dato,tiene un tamaño finito
+    //crear un vector en JAVA
+    int vec[]= new int [8];
+    //Va almacenar enteros,primero los defino como entero y con new pido memoria 8 espacios
+    //Como acceder  unvector de en una poscion determinada, usando java si quiero acceder elemento de un vector seria vec[2]
+    //Como asigno  el valor de vector usando java nombre_vector[posicion]=valor;
+    //Ejemplo vec[4]=9; para guardar  en el indice 4 el numero 9
+
+
+    
+}
+ 
