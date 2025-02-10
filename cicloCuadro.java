9edb8393a12813e3764bdfefb5cf57ffc62592f5
unknown <juanito2103@hotmail.es>
2025-02-10 09:32:31 -0500

    decimo commit

--- cicloCuadro.java
+++ cicloCuadro.java
@@ -3,6 +3,13 @@ import javax.swing.*;
 public class cicloCuadro {
     public static void main(String[] args) {
         int x = 1;
+
         String numeros = "";
+        while (x <= 10) {
+            x++;
+            numeros = numeros + x + "\n";
+
+        }
+        JOptionPane.showMessageDialog(null, numeros);
     }
 }
