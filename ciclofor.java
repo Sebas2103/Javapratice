6d0a4735241140540e5d60df83302ea966a9c333
unknown <juanito2103@hotmail.es>
2025-02-10 09:37:55 -0500

    undecimo  commit

--- ciclofor.java
+++ ciclofor.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+//Diseñar un algoritmo que calcule la  sumade n numeros ingresados por teclado. El usuario deve especificar elvalor de n 
 public class ciclofor {
-     public static void main(String[] args) {
-        
-     }
+
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n ;
+        float num,sum=0;
+        sc.close();
+    }
 }
