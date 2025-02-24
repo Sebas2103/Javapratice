e08b7c792a9a7bf6dd37eccde170ab3784dbcdbe
unknown <juanito2103@hotmail.es>
2025-02-24 11:03:30 -0500

    segundo commit

---
+++ HASHMAP.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class HASHMAP {
+    public static void main(String[] args) {
+
+        // Realizar un programa que permita almacenar el codigo y el precio de una serie
+        // de productos ,modificar el precio de un producto determinado
+        HashMap<String, Float> productos = new HashMap<String, Float>();
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        float precio;
+
+        String codigo;
+
+        while (opcion != 5) {
+            System.out.println("Introduce el numero de laopcion que quieras");
+            System.out.println("1 Introducir un producto");
+            System.out.println("2 Modificar el precio");
+            System.out.println("3 Mostrar todos los productos");
+            System.out.println("4 Eliminar productos");
+            System.out.println("5 Salir");
+            opcion = sc.nextInt();
+
+        }
+        switch (opcion) {
+            case 1:
+
+                System.out.println("Introduce el codigo de producto");
+                codigo = sc.next();
+                System.out.println("introduce el precio del producto");
+                precio = sc.nextFloat();
+                guardarProducto(codigo, precio, productos);
+                break;
+
+            case 2:
+                System.out.println("Introduce el codigo del producto del que quieres cambiar precio");
+                codigo = sc.next();
+                modificarPrecio(codigo, productos);
+                break;
+
+            case 3:
+                mostrarProductos(productos);
+                break;
+            case 4:
+                System.out.println("Introduce el codigo del producto que quieres elimminar");
+                codigo = sc.next();
+                elimminarProducto(codigo, productos);
+                break;
+            case 5:
+                System.out.println("Ha salido del sistema ");
+                break;
+            default:
+                System.out.println("Tienes que introducir una opcion valida");
+        }
+
+    }
+}
--- ejercicio3panel.java
+++ ejercicio3panel.java
@@ -2,23 +2,47 @@ import javax.swing.*;
 
 public class ejercicio3panel {
     public static void main(String[] args) {
-        int n,m;
-        n=Integer.parseInt((JOptionPane.showInputDialog("ingrese el numero de las filas o de columnas  de la matriz", args)))
-        int mat[][]=new int[n][n];
+        int n, m;
+        n = Integer.parseInt(
+                (JOptionPane.showInputDialog(null, "ingrese el numero de las filas o de columnas  de la matriz")));
+        int mat[][] = new int[n][n];
 
         llenar_matriz(mat);
         mostrar_matriz(mat);
+        m = multiplicar(mat);
 
-    
-
-        
-     }
-
-    public static void llenar_matriz(int mat[][]) {
+        JOptionPane.showMessageDialog(null, "la multiplicacion de la diagonal es" + m);
 
     }
 
-    public static void mostrar_matriz(int mat[][]) {
+    public static void llenar_matriz(int a[][]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                a[i][j] = Integer.parseInt((JOptionPane.showInputDialog(null, "ingrese un numero en la fila")));
+            }
+        }
 
+    }
+
+    public static void mostrar_matriz(int a[][]) {
+        String cad = "";
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                cad += Integer.toString(a[i][j]) + " ";
+
+            }
+
+            cad += "\n";
+        }
+        JOptionPane.showMessageDialog(null, cad);
+    }
+
+    public static int multiplicar(int a[][]) {
+        int multi = 1;
+        for (int i = 0; i < a.length; i++) {
+            multi = multi * a[i][i];
+
+        }
+        return multi;
     }
 }
